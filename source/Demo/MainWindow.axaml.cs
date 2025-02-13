e06e2e5a876a0a188ade83f4c4a513dd103f5479
Tom Laird-McConnell <thermous@iciclecreek.com>
2025-02-12 20:45:24 -0600

    add spin

--- source/Demo/MainWindow.axaml.cs
+++ source/Demo/MainWindow.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
 using Iciclecreek.Avalonia.WindowManager;
 using System;
 
@@ -8,24 +9,7 @@ namespace Demo
 {
     public partial class MainWindow : Window
     {
-        private static IImmutableSolidColorBrush[] brushes =
-        [
-            Brushes.LightBlue,
-            Brushes.LightGreen,
-            Brushes.LightYellow,
-            Brushes.LightCyan,
-            Brushes.LightGray,
-            Brushes.LightPink,
-            Brushes.LightSalmon,
-            Brushes.LightSeaGreen,
-            Brushes.LightSkyBlue,
-            Brushes.LightSlateGray,
-            Brushes.LightSteelBlue,
-            Brushes.LightGoldenrodYellow,
-            Brushes.LightGreen,
-            Brushes.LightPink,
-            Brushes.LightSalmon,
-        ];
+        
 
         private int _counter = 3;
 
@@ -40,10 +24,8 @@ namespace Demo
             var height = Random.Shared.Next(100, 400);
             var position = new PixelPoint(Random.Shared.Next(0, (int)Bounds.Width - width),
                                           Random.Shared.Next(0, (int)Bounds.Height - height));
-            var window = new ManagedWindow()
+            var window = new MyManagedWindow()
             {
-                Title = "New Window",
-                Background = brushes[_counter++ % brushes.Length],
                 Width = width,
                 Height = height,
                 Position = position,
@@ -52,4 +34,5 @@ namespace Demo
             WindowsPanel.ShowWindow(window);
         }
     }
+
 }
---
+++ source/Demo/MyManagedWindow.axaml.cs
@@ -0,0 +1,94 @@
+using Avalonia.Animation.Easings;
+using Avalonia.Animation;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Styling;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Iciclecreek.Avalonia.WindowManager;
+using System;
+
+namespace Demo;
+
+public partial class MyManagedWindow : ManagedWindow
+{
+    private static int _windowCount = 0;
+    private static IImmutableSolidColorBrush[] brushes =
+        [
+            Brushes.DarkBlue,
+            Brushes.DarkGreen,
+            Brushes.DarkCyan,
+            Brushes.DarkGoldenrod,
+            Brushes.DarkOrchid,
+            Brushes.DarkOrange,
+            Brushes.DarkGray,
+            Brushes.DarkSalmon,
+            Brushes.DarkSeaGreen,
+            Brushes.DarkSlateGray,
+            Brushes.DarkSlateBlue,
+            Brushes.DarkTurquoise,
+            Brushes.DarkViolet,
+            Brushes.DarkKhaki,
+            Brushes.DarkOliveGreen,
+        ];
+
+    public MyManagedWindow()
+    {
+        InitializeComponent();
+
+        this.Background = brushes[_windowCount % brushes.Length];
+
+        this.DataContext = new MyManagedWindowViewModel()
+        {
+            Counter = 0,
+            Title = $"New Window {++_windowCount}"
+        };
+    }
+
+    private void OnIncrement(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var vm = (MyManagedWindowViewModel)this.DataContext;
+        vm.Counter++;
+    }
+
+    private void OnSpin(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Create a rotate transform and apply it to the control
+
+        var rotateAnimation = new Animation
+        {
+            Duration = TimeSpan.FromSeconds(1),
+            Easing = new SineEaseInOut(),
+            Children =
+                    {
+                        new KeyFrame
+                        {
+                            Cue = new Cue(0d),
+                            Setters =
+                            {
+                                new Setter(RotateTransform.AngleProperty, 0d)
+                            }
+                        },
+                        new KeyFrame
+                        {
+                            Cue = new Cue(1d),
+                            Setters =
+                            {
+                                new Setter(RotateTransform.AngleProperty, 360d)
+                            }
+                        }
+                    }
+        };
+
+        rotateAnimation.RunAsync(this);
+    }
+
+}
+
+public partial class MyManagedWindowViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private int _counter = 0;
+
+    [ObservableProperty]
+    private string _title = "New Window";
+}
