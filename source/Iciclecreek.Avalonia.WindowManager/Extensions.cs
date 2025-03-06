a6f319035c60585c7543d997a5d92a2a6de34e43
Tom Laird-McConnell <thermous@iciclecreek.com>
2025-03-06 11:26:48 -0800

    dialogs

--- source/Iciclecreek.Avalonia.WindowManager/Extensions.cs
+++ source/Iciclecreek.Avalonia.WindowManager/Extensions.cs
@@ -1,9 +1,13 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+using Avalonia.Media;
 using Avalonia.VisualTree;
 using System;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace Iciclecreek.Avalonia.WindowManager
 {
@@ -31,12 +35,55 @@ namespace Iciclecreek.Avalonia.WindowManager
     {
         public static void ShowWindow(this Visual visual, ManagedWindow window)
         {
-            WindowsPanel windowManager = visual as WindowsPanel ??
+            WindowsPanel? windowManager = visual as WindowsPanel ??
                 visual.FindAncestorOfType<WindowsPanel>() ??
                 TopLevel.GetTopLevel(visual).FindDescendantOfType<WindowsPanel>();
 
             ArgumentNullException.ThrowIfNull(windowManager, nameof(WindowsPanel));
             windowManager.AddWindow(window);
+        }
+
+        public static void ShowDialog(this Visual visual, ManagedWindow window)
+        {
+            WindowsPanel? windowsPanel = visual as WindowsPanel ??
+                visual.FindAncestorOfType<WindowsPanel>() ??
+                TopLevel.GetTopLevel(visual).FindDescendantOfType<WindowsPanel>();
+
+            ArgumentNullException.ThrowIfNull(windowsPanel, nameof(WindowsPanel));
+
+            ManagedWindow owner = visual.FindAncestorOfType<ManagedWindow>()
+                ?? CreateOverlayWindow(windowsPanel, window);
+
+            window.ShowDialog(owner);
+        }
+
+        private static ManagedWindow CreateOverlayWindow(WindowsPanel wm, ManagedWindow childWindow)
+        {
+            var overlay = new ManagedWindow()
+            {
+                Position = new PixelPoint((ushort)wm.Bounds.Left, (ushort)wm.Bounds.Top),
+                SystemDecorations = SystemDecorations.None,
+                Background = Brushes.Transparent,
+                ShowActivated = false,
+                Width = wm.Bounds.Width,
+                Height = wm.Bounds.Height,
+                AnimateWindow = false
+            };
+            EventHandler<SizeChangedEventArgs> OnResized = (_, _) =>
+            {
+                overlay.Width = wm.Bounds.Width;
+                overlay.Height = wm.Bounds.Height;
+            };
+            wm.SizeChanged += OnResized;
+
+            childWindow.Closed += (_, _) =>
+            {
+                wm.SizeChanged -= OnResized;
+                overlay.Close();
+            };
+
+            wm.AddWindow(overlay);
+            return overlay;
         }
     }
 }
