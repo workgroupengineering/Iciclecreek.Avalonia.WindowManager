5d6a9215ef586ec9b7ec1acfd882d07a824714e5
Evgeny Gorbovoy <goe5mu@bosch.com>
2025-07-22 23:40:03 +0200

    fix event handling in ManagedWindow

--- source/Iciclecreek.Avalonia.WindowManager/ManagedWindow.axaml.cs
+++ source/Iciclecreek.Avalonia.WindowManager/ManagedWindow.axaml.cs
@@ -1119,6 +1119,8 @@ public class ManagedWindow : OverlayPopupHost
 
             if (!IsActive)
                 Activate();
+
+            e.Handled = true;
         };
 
         partTitleBar.PointerReleased += (object? sender, PointerReleasedEventArgs e) =>
@@ -1129,10 +1131,11 @@ public class ManagedWindow : OverlayPopupHost
                 {
                     SetPsuedoClasses(false);
                     start = null;
+                    e.Handled = true;
                 }
             }
         };
-
+        
         partTitleBar.PointerMoved += (object? sender, PointerEventArgs e) =>
         {
             if (WindowState != WindowState.Maximized)
@@ -1146,6 +1149,7 @@ public class ManagedWindow : OverlayPopupHost
                     var delta = position - start.Value;
                     start = position;
                     this.Position = this.Position + delta;
+                    e.Handled = true;
                 }
             }
         };
@@ -1153,6 +1157,9 @@ public class ManagedWindow : OverlayPopupHost
         partTitleBar.PointerCaptureLost += (object? sender, PointerCaptureLostEventArgs e) =>
         {
             SetPsuedoClasses(false);
+            if(start!=null)
+                e.Handled = true;
+            
             start = null;
         };
 
@@ -1448,6 +1455,8 @@ public class ManagedWindow : OverlayPopupHost
                 WindowState = WindowState.Maximized;
             else if (WindowState == WindowState.Maximized)
                 WindowState = WindowState.Normal;
+
+            e.Handled = true;
         }
     }
 
